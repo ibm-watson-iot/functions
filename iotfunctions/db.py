7c5ae81783ba96244229ce1b456bec746821cc41
bryantsai <bryan@bryantsai.com>
2019-01-14 21:14:21 +0800

    Added engine input API to db.http_request.

--- iotfunctions/db.py
+++ iotfunctions/db.py
@@ -260,6 +260,7 @@ class Database(object):
         self.url[('granularitySet','POST')] = '/'.join([base_url,'kpi','v1',self.tenant_id,'entityType',object_name,object_type])
         self.url[('granularitySet','DELETE')] = '/'.join([base_url,'kpi','v1',self.tenant_id,'entityType',object_name,object_type,object_name_2])
         self.url[('granularitySet','GET')] = '/'.join([base_url,'kpi','v1',self.tenant_id,'entityType',object_name,object_type])
+        self.url[('engineInput','GET')] = '/'.join([base_url,'kpi','v1',self.tenant_id,'entityType',object_type])
         encoded_payload = json.dumps(payload).encode('utf-8')        
         headers = {
             'Content-Type': "application/json",
