58fc8f053e8f56ff42ed78bd54e97edaaddcaeb7
Leucir <lmarinj@us.ibm.com>
2019-01-14 19:38:47 -0600

    COS adding create bucket method

--- iotfunctions/db.py
+++ iotfunctions/db.py
@@ -309,6 +309,17 @@ class Database(object):
             logger.info('Not able to DELETE %s to COS bucket %s', (filename, bucket))
         return ret
 
+    def cos_create_bucket(self, bucket=None):
+        if bucket is None:
+            logger.info('Not able to CREATE the bucket. A name should be provided.')
+        if self.cos_client is not None:
+            ret = self.cos_client.cos_put(key=None, payload=None, bucket=bucket)
+        else:
+            ret = None
+        if ret is None:
+            logger.info('Not able to CREATE the bucket %s.'% bucket)
+        return ret
+
     def commit(self):
         '''
         Commit the active session
