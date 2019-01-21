2e8c25ca74886a6461d8eea2c603e13ae0532755
Michael Adendorff <Michael.Adendorff@ca.ibm.com>
2019-01-21 10:00:05 -0800

    version change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='iotfunctions',
-    version='1.1.2',
+    version='1.1.3',
     packages=find_packages(),
     install_requires=['dill'
     ],
