82e16d689d361e58ba8227920578b1d5cf093d0e
Michael Adendorff <Michael.Adendorff@ca.ibm.com>
2019-04-09 15:15:32 -0700

    added sample for constants

---
+++ scripts/sample_constants.py
@@ -0,0 +1,55 @@
+'''
+Sample script demonstrates how to register new Analytic Service constants.
+
+Constants are parameters that can be managed in the Analytic Service UI and
+used within expressions and custom functions.
+
+'''
+#import open source libraries
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+import pandas as pd
+import json
+
+#supply credentials
+with open('credentials.json', encoding='utf-8') as F:
+    credentials = json.loads(F.read())
+
+# import classes needed from iotfunctions
+from iotfunctions.db import Database
+from iotfunctions.ui import UISingle,UIMulti
+
+# Connect to Analytic Service
+db = Database(credentials = credentials, tenant_id=credentials['tennant_id'])
+
+
+'''
+Constants are defined by indentifying the UI control that will manager them.
+Constants may either be scalars or arrays. Single valued (scalars) will be
+managed using single line edit, which is derived from the UISingle class.
+
+'''
+gamma = UISingle(name='gamma',
+                 description= 'Sample single valued parameter',
+                 datatype=float)
+
+'''
+Arrays are manageed by a Multi-valued 
+'''
+
+zeta = UIMulti(name='zeta',
+                 description= 'Sample multi-valued array',
+                 values = ['A','B','C'],
+                 datatype = str
+            )
+
+'''
+Use the register_constants method on the Database object to make constants
+available in the UI
+'''
+db.register_constants([zeta])
+
+
+
+
