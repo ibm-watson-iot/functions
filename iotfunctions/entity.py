59a7d77ae2a3e1efc738aa2de32bf2a2d61d1f0f
Sivakumar Rajendran <rsiva@us.ibm.com>
2019-04-25 15:13:05 -0500

    comment out one kpi function

--- iotfunctions/entity.py
+++ iotfunctions/entity.py
@@ -56,7 +56,7 @@ class Boiler(EntityType):
         args.append(Column('fuel_flow_rate',Float()))
         args.append(Column('air_flow_rate',Float()))
         args.append(bif.IoTEntityDataGenerator(ids=None))
-        args.append(bif.TimestampCol(dummy_items = None, output_item = 'timestamp_col'))
+        # args.append(bif.TimestampCol(dummy_items = None, output_item = 'timestamp_col'))
         args.append(bif.DateDifferenceReference(date_1='timestamp_col',ref_date=dt.datetime.utcnow()))
         kw = {'_timestamp' : timestamp,
               '_db_schema' : db_schema,
