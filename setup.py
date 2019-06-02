715df6e6d5c1b34215cb35b513d7788a01464a64
Michael Adendorff <mike@adendorff.com>
2019-06-02 14:39:45 -0700

    version change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='iotfunctions',
-    version='2.0.1',
+    version='2.0.2',
     packages=find_packages(),
     install_requires=['dill'
     ],
