a2a91b911b3cf2109e95152d6babe63f8782e8d3
Markus Müller <markus_mueller@de.ibm.com>
2019-10-18 20:27:12 +0200

    forgot pyod

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ setup(
         'ibm-cos-sdk==2.1.3',
         'scipy==1.1.0',
         'scikit-learn==0.19.2',
-        'scikit-learn==0.16,1'
+        'scikit-learn==0.16.1',
+        'pyod==0.7.5'
     ],
     extras_require = {
         'kafka':  ['confluent-kafka==0.11.5']
