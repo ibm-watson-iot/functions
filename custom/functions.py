31e45f3230f8c6e0c835ffc857932ad6776b3b85
Amritha raj herle <herleraja@gmail.com>
2019-12-04 12:59:21 +0100

    package url change

--- custom/functions.py
+++ custom/functions.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 # Specify the URL to your package here.
 # This URL must be accessible via pip install
 
-PACKAGE_URL = 'git+https://github.com/madendorff/functions@'
+PACKAGE_URL = 'git+https://github.com/<path_to_repository>@'
 
 
 class HelloWorld(BaseTransformer):
