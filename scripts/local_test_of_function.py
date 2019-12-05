be0fc8ec2354d4448395a1d206074cbde76a6023
Amritha raj herle <herleraja@gmail.com>
2019-12-05 11:13:34 +0100

    Updating the test script.

--- scripts/local_test_of_function.py
+++ scripts/local_test_of_function.py
@@ -4,10 +4,7 @@ import pandas as pd
 import numpy as np
 import logging
 from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, func
-from iotfunctions.base import BaseTransformer
-from iotfunctions.metadata import EntityType
 from iotfunctions.db import Database
-from iotfunctions import ui
 from iotfunctions.enginelog import EngineLogging
 
 EngineLogging.configure_console_logging(logging.DEBUG)
@@ -16,8 +13,8 @@ EngineLogging.configure_console_logging(logging.DEBUG)
 You can test functions locally before registering them on the server to
 understand how they work.
 
-In this script I am using a function from iotfunctions, but you can
-do this with any function derived from the iotfunctions base classes.
+Supply credentials by pasting them from the usage section into the UI.
+Place your credentials in a separate file that you don't check into the repo. 
 
 '''
 
@@ -27,7 +24,7 @@ db_schema = None
 db = Database(credentials=credentials)
 
 '''
-Import and instantiate the functions to be tested
+Import and instantiate the functions to be tested 
 
 The local test will generate data instead of using server data.
 By default it will assume that the input data items are numeric.
@@ -42,30 +39,15 @@ This file will be written to the working directory.
 
 '''
 
-from iotfunctions.bif import AlertExpression
-
-fn = AlertExpression(expression='df["x1"] > 1', alert_name='is_high_x1')
-fn.execute_local_test(db=db, db_schema=db_schema)
-
-from iotfunctions.bif import DateDifference
-
-fn = DateDifference(date_1='d1', date_2='d2', num_days='difference')
+from custom.functions import HelloWorld
+fn = HelloWorld(
+        name = 'AS_Tester',
+        greeting_col = 'greeting')
+fn.execute_local_test(db=db,db_schema=db_schema)
 
 '''
-This function requires date imputs. To indicate that the function should
-be tested using date inputs declare two date columns as below.
-
+Register function so that you can see it in the UI
 '''
 
-fn.execute_local_test(columns=[Column('d1', DateTime), Column('d2', DateTime)])
+db.register_functions([HelloWorld])
 
-'''
-If the function that you are testing requires assess to server resources,
-pass a Database object
-'''
-
-from iotfunctions.bif import SaveCosDataFrame
-
-fn = SaveCosDataFrame(filename='test_df_write', columns=['x1', 'x2'], output_item='wrote_df')
-
-fn.execute_local_test(db=db, db_schema=db_schema)
