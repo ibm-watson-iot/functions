27b00286522990de708cf9de9887bcfa2b6a36c0
kohlmann <kohlmann@de.ibm.com>
2020-01-08 10:02:08 +0100

    Handle maintenance periods with identical start dates

--- iotfunctions/base.py
+++ iotfunctions/base.py
@@ -2041,9 +2041,9 @@ class BaseDBActivityMerge(BaseDataSource):
         c.name = self._activity
         c.index.name = self._start_date
         # use original data to update the new set of intervals in slices
-        for df_row in df.itertuples():
-            end_date = getattr(df_row, self._end_date) - dt.timedelta(milliseconds=1)
-            c[getattr(df_row, self._start_date):end_date] = getattr(df_row, self._activity)
+        for df_row in df[[self._start_date, self._end_date, self._activity]].itertuples(index=False, name=None):
+            end_date = df_row[1] - dt.timedelta(milliseconds=1)
+            c[df_row[0]:end_date] = df_row[2]
         df = c.to_frame()
         df.reset_index(inplace=True)
         df.index.name = self.auto_index_name
