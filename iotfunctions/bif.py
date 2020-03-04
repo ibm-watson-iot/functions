4a3dc25b0d04f4f591708817d1aeda0bcb055acc
Ankit Jha <jhaa@us.ibm.com>
2020-03-03 20:43:35 -0600

    issue#527 : AnomalyGeneratorFlatline fixes for marking logic

--- iotfunctions/bif.py
+++ iotfunctions/bif.py
@@ -801,12 +801,12 @@ class AnomalyGeneratorExtremeValue(BaseTransformer):
                     #Mark anomaly point
                     timeseries[self.output_item].iloc[grp_row_index] = np.random.choice([-1, 1]) * self.size * local_std
                     logger.debug('Anomaly Index Value{}'.format(grp_row_index))
-            
+
             counts_by_entity_id[entity_grp_id] = count
 
         logger.debug('Final Grp Counts {}'.format(counts_by_entity_id))
 
-        #Save the group counts to cos
+        #Save the group counts to db
         db.model_store.store_model(key, counts_by_entity_id)
 
         timeseries.set_index(df.index.names,inplace=True)
@@ -1002,8 +1002,7 @@ class AnomalyGeneratorFlatline(BaseTransformer):
 
             entity_grp_id = grp[0]
             df_entity_grp = grp[1]
-            logger.debug('Group id {}'.format(grp[0]))
-            logger.debug('Group Indexes {}'.format(df_entity_grp.index))
+            logger.debug('Group {} Indexes {}'.format(grp[0],df_entity_grp.index))
 
             count = 0
             width = self.width
@@ -1011,32 +1010,36 @@ class AnomalyGeneratorFlatline(BaseTransformer):
             if entity_grp_id in counts_by_entity_id:
                 count = counts_by_entity_id[entity_grp_id][0]
                 width = counts_by_entity_id[entity_grp_id][1]
-                if counts_by_entity_id[entity_grp_id][2] is not None:
+                if count != 0:
                     local_mean = counts_by_entity_id[entity_grp_id][2]
 
             mark_anomaly = False
             for grp_row_index in df_entity_grp.index:
                 count += 1
-                if count%self.factor == 0:
+                
+                if width!=self.width or count%self.factor == 0:
                     #Start marking points
                     mark_anomaly = True
 
                 if mark_anomaly:
-                    timeseries.iloc[grp_row_index] = local_mean
+                    timeseries[self.output_item].iloc[grp_row_index] = local_mean
                     width -= 1
                     logger.debug('Anomaly Index Value{}'.format(grp_row_index))
 
                 if width==0:
                     #End marking points
-                    mark_anomaly =False
+                    mark_anomaly = False
+                    #Update values
                     width = self.width
-                    local_mean = None
+                    count = 0
+                    local_mean = df_entity_grp.iloc[:10][self.input_item].mean()
 
-                counts_by_entity_id[entity_grp_id] = (count,width,local_mean)
+            counts_by_entity_id[entity_grp_id] = (count,width,local_mean)
+
 
         logger.debug('Final Grp Counts {}'.format(counts_by_entity_id))
 
-        #Save the group counts to ModelStore
+        #Save the group counts to db
         db.model_store.store_model(key, counts_by_entity_id)
 
         timeseries.set_index(df.index.names,inplace=True)
