2fce3a13509693e6f06805c16d2bbc2fa1283f68
Markus Müller <markus_mueller@de.ibm.com>
2020-04-21 19:58:07 +0200

    update train lightgbm

--- scripts/train_lightgbm_regressor.py
+++ scripts/train_lightgbm_regressor.py
@@ -10,6 +10,7 @@ from iotfunctions.anomaly import GBMRegressor
 # from mmfunctions.anomaly import GBMRegressor
 from iotfunctions.dbtables import DBModelStore
 from iotfunctions import pipeline as pp
+from sqlalchemy import Column, Float
 
 import datetime as dt
 import ibm_db
@@ -20,24 +21,26 @@ with open('credentials_as_dev.json', encoding='utf-8') as F:
 EngineLogging.configure_console_logging(logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-
-def main(argv):
-
-    # entityType = 'Clients04'
-    entityType = ''
-    featureC = 'pressure'
-    targetC = 'temperature'
-    predictC = 'predict'
-    startTime = None
-    endTime = None
-    startTimeV = 0
-    endTimeV = 0
-    helpString = 'train.py -E <entityType> -f <feature column> -t <target column> -p <prediction column> \
+db = None
+db_connection = None
+entityType = ''
+featureC = 'pressure'
+targetC = 'temperature'
+predictC = 'predict'
+metric = None
+startTime = None
+endTime = None
+startTimeV = 0
+endTimeV = 0
+helpString = 'train.py -E <entityType> -f <feature column> -t <target column> -p <prediction column> -m <metric> \
 -s <starttime> -e <endtime>'
 
+
+def get_options(argv):
+    global db, db_connection, entityType, featureC, targetC, predictC, metric, startTime, endTime, startTimeV, endTimeV, helpString
     try:
         opts, args = getopt.getopt(
-            argv, "hf:t:p:s:e:E:", ["featureC=", "targetC=", "predictC=", "startTime=", "endTime=", "entityType="])
+            argv, "hf:t:p:m:s:e:E:", ["featureC=", "targetC=", "predictC=", "metric=", "startTime=", "endTime=", "entityType="])
     except getopt.GetoptError as ge:
         print(str(ge))
         print(helpString)
@@ -54,6 +57,8 @@ def main(argv):
             targetC = arg
         elif opt in ("-p", "--predict"):
             predictC = arg
+        elif opt in ("-m", "--metric"):
+            metric = arg
         elif opt in ("-s", "--starttime"):
             startTime = arg
         elif opt in ("-e", "--endtime"):
@@ -62,13 +67,24 @@ def main(argv):
     print('Feature Column (X) "', featureC)
     print('Target Column (Y) "', targetC)
     print('Predictor Column "', predictC)
+    print('Metric Name"', metric)
     print('StartTime "', startTime)
     print('EndTime "', endTime)
+
+    return
+
+
+def main(argv):
+    global db, db_connection, entityType, featureC, targetC, predictC, metric, startTime, endTime, startTimeV, endTimeV, helpString
+    get_options(argv)
 
     if entityType == '':
         print('entityType name is missing')
         print(helpString)
         sys.exit(3)
+
+    if metric != '':
+        print('ditch feature, target and predicted values and get it from definition - this is the safest way')
 
     # endTime == None means now
     if endTime is None:
@@ -100,24 +116,53 @@ def main(argv):
     model_store = DBModelStore(credentials['tenantId'], entityType, credentials['db2']['username'], db_connection, 'db2')
     db.model_store = model_store
 
-    meta = db.get_entity_type(entityType)
+    # with open('output.json', 'w+', encoding='utf-8') as G:
+    #    json.dump(db.entity_type_metadata, G)
 
     logger.info('Connected to database - SQL alchemy and native')
 
-    gbm = GBMRegressor(features=[featureC], targets=[targetC], threshold=2, predictions=[predictC])
+    meta = None
+    try:
+        meta = db.get_entity_type(entityType)
+        print(meta)
+    except Exception:
+        logger.error('Failed to retrieve information about entityType ' + str(entityType) + ' from the database')
+
+    # make sure the results of the python expression is saved to the derived metrics table
+    if metric == '':
+        # take the first suitable choice if there is no metric
+        sourceTableName = ''
+        for di in meta['dataItemDto']:
+            sourceTableName = di['sourceTableName']
+            if len(sourceTableName) > 0:
+                break
+        if len(sourceTableName) > 0:
+            meta._data_items.append({'columnName': predictC, 'columnType': 'NUMBER', 'kpiFunctionId': 22856,
+                                     'kpiFunctionDto': {'output': {'name': predictC}},
+                                     'name': predictC, 'parentDataItemName': None, 'sourceTableName': sourceTableName,
+                                     'tags': {}, 'transient': True, 'type': 'DERIVED_METRIC'})
+        else:
+            logger.error('No suitable derived metric table found')
+            return
+    else:
+        found = False
+        try:
+            for di in meta['dataItemDto']:
+                if di.name == metric:
+                    found = True
+                    predictC = di.columnName
+                    break
+            if not found:
+                logger.error('Metric does not exist')
+                return
+        except Exception:
+            pass
+
+    gbm = GBMRegressor(features=[featureC], targets=[targetC], predictions=[predictC])
+
     # max_depth=20, num_leaves=40, n_estimators=4000, learning_rate=0.00001)
 
     gbm.delete_existing_models = True
-
-    meta._functions = [gbm]
-
-    logger.info('Created Regressor')
-
-    # make sure the results of the python expression is saved to the derived metrics table
-    meta._data_items.append({'columnName': predictC, 'columnType': 'NUMBER', 'kpiFunctionId': 22856,
-                             'kpiFunctionDto': {'output': {'name': predictC}},
-                             'name': predictC, 'parentDataItemName': None, 'sourceTableName': 'DM_CLIENTS04',
-                             'tags': {}, 'transient': True, 'type': 'DERIVED_METRIC'})
 
     jobsettings = {'db': db,
                    '_production_mode': False,
@@ -125,6 +170,15 @@ def main(argv):
                    '_end_ts_override': (dt.datetime.utcnow() - dt.timedelta(days=endTimeV)),
                    '_db_schema': 'BLUADMIN',
                    'save_trace_to_file': True}
+
+    if meta is not None:
+        meta._functions = [gbm]
+    else:
+        meta = gbm._build_entity_type(columns = [Column(featureC, Float()), # Column('accel_power_1',Float()),
+                                                 Column(predictC, Float())], **jobsettings)
+        gbm._entity_type = meta
+
+    logger.info('Created Regressor')
 
     logger.info('Instantiated training job')
 
@@ -138,3 +192,7 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+    if db_connection is not None:
+        db_connection.disconnect()
+    if db is not None:
+        db.close()
