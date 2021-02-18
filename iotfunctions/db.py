31c653906d4ddf34ac977d8666781192d6a17a1e
shraddha-singh <shraddha-singh@ibm.com>
2021-02-18 17:35:34 -0600

    [patch] make end_ts inclusive in where clause

--- iotfunctions/db.py
+++ iotfunctions/db.py
@@ -2031,7 +2031,7 @@ class Database(object):
             if timestamp_col is None:
                 msg = 'No timestamp_col provided to query. Must provide a timestamp column if you have a date filter'
                 raise ValueError(msg)
-            query = query.filter(self.get_column_object(table, timestamp_col) < end_ts)
+            query = query.filter(self.get_column_object(table, timestamp_col) <= end_ts)
         if not entities is None:
             query = query.filter(table.c[deviceid_col].in_(entities))
 
@@ -2311,7 +2311,7 @@ class Database(object):
                 if timestamp is None:
                     msg = 'No timestamp_col provided to query. Must provide a timestamp column if you have a date filter'
                     raise ValueError(msg)
-                subquery = subquery.filter(self.get_column_object(table, timestamp) < end_ts)
+                subquery = subquery.filter(self.get_column_object(table, timestamp) <= end_ts)
             if not entities is None:
                 subquery = subquery.filter(table.c[deviceid_col].in_(entities))
             for d, members in list(filters.items()):
@@ -2574,7 +2574,7 @@ class Database(object):
                 if timestamp is None:
                     msg = 'No timestamp_col provided to query. Must provide a timestamp column if you have a date filter'
                     raise ValueError(msg)
-                subquery = subquery.filter(self.get_column_object(table, timestamp) < end_ts)
+                subquery = subquery.filter(self.get_column_object(table, timestamp) <= end_ts)
             if not entities is None:
                 subquery = subquery.filter(table.c[deviceid_col].in_(entities))
             for d, members in list(filters.items()):
