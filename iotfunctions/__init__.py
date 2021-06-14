4f81c9e18f79e2c81e56c09ddee9f47ca607d14c
kohlmann <kohlmann@de.ibm.com>
2021-06-14 11:03:36 +0200

    copyright corrections

--- iotfunctions/__init__.py
+++ iotfunctions/__init__.py
@@ -1,3 +1,13 @@
+# *****************************************************************************
+# © Copyright IBM Corp. 2018.  All Rights Reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the Apache V2.0 license
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# *****************************************************************************
+
 import os
 import pkgutil
 
--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+# *****************************************************************************
+# © Copyright IBM Corp. 2018.  All Rights Reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the Apache V2.0 license
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# *****************************************************************************
 
 from setuptools import setup, find_packages
 
