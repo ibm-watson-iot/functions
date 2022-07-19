f94aa35ce1a2881682b6d5ebc99c22fad9f92554
Sivakumar Rajendran <rsiva@us.ibm.com>
2022-07-19 08:16:22 -0400

    update setup to 8.7.x

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@
 from setuptools import setup, find_packages
 
 setup(name='custom', version='0.0.1', packages=find_packages(),
-      install_requires=['iotfunctions@git+https://github.com/ibm-watson-iot/functions.git@production'])
+      install_requires=['iotfunctions@git+https://github.com/ibm-watson-iot/functions.git@8.7.x'])
