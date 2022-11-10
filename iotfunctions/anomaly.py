bdee5b4e3382093e2d05bb59730c974716b5444b
Markus Müller <markus_mueller@de.ibm.com>
2022-11-10 10:58:46 +0100

    Make sure shape is correct in AnomalyScorer

--- iotfunctions/anomaly.py
+++ iotfunctions/anomaly.py
@@ -666,7 +666,11 @@ class AnomalyScorer(BaseTransformer):
                     else:
                         zScoreII = scores[i]
 
-                    df[output_item] = zScoreII
+                    # make sure shape is correct
+                    try:
+                        df[output_item] = zScoreII
+                    except Exception as e2:                    
+                        df[output_item] = zScoreII.reshape(-1,1)
 
             except Exception as e:
                 logger.error(self.whoami + ' score integration failed with ' + str(e))
