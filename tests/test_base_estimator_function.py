f78183d69e50f533047b36c886c5e3215a73443f
KOHLMANN-de <kohlmann@de.ibm.com>
2025-01-10 16:37:31 +0100

    [patch] replace 'ls' by 'squared_error'
    (cherry picked from commit e6f83a4f39391568376af3f1823fab7e60e08d5d)

--- tests/test_base_estimator_function.py
+++ tests/test_base_estimator_function.py
@@ -55,7 +55,7 @@ class TestRegressor(BaseEstimatorFunction):
     def set_estimators(self):
     # gradient_boosted
         params = {'n_estimators': [100, 250, 500], 'max_depth': [2, 4, 10], 'min_samples_split': [2, 5, 9],
-                  'learning_rate': [0.01, 0.02, 0.05], 'loss': ['ls']}
+                  'learning_rate': [0.01, 0.02, 0.05], 'loss': ['squared_error']}
         self.estimators['gradient_boosted_regressor'] = (ensemble.GradientBoostingRegressor, params)
         # sgd
         params = {'max_iter': [250, 1000, 5000, 10000], 'tol': [0.001, 0.002, 0.005]}
