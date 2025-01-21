234243198a2d478b827375110be849f28090da62
KOHLMANN-de <kohlmann@de.ibm.com>
2025-01-21 11:23:39 +0100

    [patch] fix generation of sample entity type II
    (cherry picked from commit e33f18ef21f267350f12fd8513c379baf0830dd4)

--- iotfunctions/metadata.py
+++ iotfunctions/metadata.py
@@ -393,7 +393,7 @@ class EntityType(object):
         try:
             sqltable = self.db.get_table(name, self._db_schema)
         except KeyError:
-            table.create()
+            table.create(self.db.engine)
         self.activity_tables[name] = table
 
     def add_slowly_changing_dimension(self, property_name, datatype, **kwargs):
@@ -429,7 +429,7 @@ class EntityType(object):
         try:
             self.db.get_table(name, self._db_schema)
         except KeyError:
-            table.create()
+            table.create(self.db.engine)
         self.scd[property_name] = table
 
     def _add_scd_pipeline_stage(self, scd_lookup):
@@ -2159,7 +2159,7 @@ class EntityType(object):
             table = self.db.get_table(self._dimension_table_name, self._db_schema)
             for i in new_ids:
                 stmt = table.insert().values({self._entity_id: i})
-                self.db.connection.execute(stmt)
+                self.db.engine.execute(stmt)
             self.db.commit()
             return new_ids
 
